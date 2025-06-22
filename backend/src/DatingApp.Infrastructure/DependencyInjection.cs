903bf1fecefd0f21c174ebc81b412560069fd919
Nazarii Shypka <nazarii.shypka.pz.2022@lpnu.ua>
2025-06-23 00:35:46 +0300

    Implement base API infrastructure

---
+++ backend/src/DatingApp.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using DatingApp.Infrastructure.Data;
+
+namespace DatingApp.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddDbContext<DataContext>(opt =>
+        {
+            opt.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+        });
+
+        return services;
+    }
+}
